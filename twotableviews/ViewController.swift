26e0c82756696aaa617e2ecbddfe98b43f1e6a73
cristinabarclay <57467401+cristinabarclay@users.noreply.github.com>
2020-03-02 22:29:14 -0600

    Initial Commit

---
+++ twotableviews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  twotableviews
+//
+//  Created by Cristina Barclay on 3/2/20.
+//  Copyright © 2020 Cristina Barclay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
